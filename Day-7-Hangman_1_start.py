559f0658e8b965c8112baf8dbbe7134a7a58bea1
McQuest-72 <mcquest2347@gmail.com>
2024-04-17 10:17:53 -0800

    Hangman game program is created

---
+++ Day-7-Hangman_1_start.py
@@ -0,0 +1,14 @@
+
+"""This is a game program called hangman"""
+
+# Step 1
+
+word_list = ['ardvark', 'baboon', 'camel']
+
+# TODO-1 - Randomly choose a word from the word_list and assign it to a variable called chosen_word.
+
+# TODO-2 - Ask the user to gues a letter and assign their answer to a variable called guess. Make guess lowercase.
+
+# TODO-3 - Check if the letter the user guessed (guess) is one of the letters in the chosen_word
+
+print(word_list)
