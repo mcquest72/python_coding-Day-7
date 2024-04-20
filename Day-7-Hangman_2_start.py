2e8581be4a84cd7552298b8377211ccd417a4cc7
McQuest-72 <mcquest2347@gmail.com>
2024-04-20 05:33:23 -0800

    All 3 TODOs are completed and code executed successfully

--- Day-7-Hangman_2_start.py
+++ Day-7-Hangman_2_start.py
@@ -14,18 +14,34 @@ print(f'Psst, the solution is {chosen_word}.')
 # For each letter in the chosen_word, add a '_' to 'display'.
 # So if the chosen_word was 'apple', display should be ['_', '_', '_', '_', '_'] with 5 '_' representing each letter to guess
 
+display = []
+for letter in chosen_word:
+    display += '_'
+print(display)
+
+# Or we can do it this way
+"""
+display = []
+for _ in range(len(chosen_word)):
+    dispaly += '_'
+print(display) 
+"""    
+
+
 guess = input('Guess a letter: ').lower()
 
 # TODO-2: - Loop through each position in the chosen_word;
-# If the letter at that position matches 'guess' then reveal that letter in the display at theat position.
+# If the letter at that position matches 'guess' then reveal that letter in the display at that position.
 # e.g. If the user guessed 'p' and the chosen_word was 'apple', then display should be ['_', 'p', 'p', '_', '_'].
 
-for letter in chosen_word:
+for position in range(len(chosen_word)):
+    letter = chosen_word[position]
     if letter == guess:
-        print('Right')
-    else:
-        print('Wrong')
+        display[position] = letter
+
 
 # TODO-3: - Print 'display' and you should see the guessed letter in the correct position and every other letter replace with '_'.
 
-# Hint - Don't worry about getting the user to guess the net letter. We'll tackle that in step 3.
+print(display)
+
+# Hint - Don't worry about getting the user to guess the next letter. We'll tackle that in step 3.
