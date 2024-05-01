1cc7bc4a09751df5348402d255496c9ea2cad430
McQuest-72 <mcquest2347@gmail.com>
2024-05-01 12:47:30 -0800

    Three TODOs code added and executed successfully.

--- Day-7-Hangman_4_Keeping_Track_of_the_Player's_Lives.py
+++ Day-7-Hangman_4_Keeping_Track_of_the_Player's_Lives.py
@@ -71,7 +71,10 @@ word_length = len(chosen_word)
 #TODO-1: - Create a variable called 'lives' to keep track of the number of lives left. 
 #Set 'lives' to equal 6.
 
+lives = 6
+
 #Testing code
+print()
 print(f'Pssst, the solution is {chosen_word}.')
 
 #Create blanks
@@ -92,6 +95,12 @@ while not end_of_game:
     #TODO-2: - If guess is not a letter in the chosen_word,
     #Then reduce 'lives' by 1. 
     #If lives goes down to 0 then the game should stop and it should print "You lose."
+    
+    if guess not in chosen_word:
+        lives -= 1
+        if lives == 0:
+            end_of_game = True
+            print('You lose.')
 
     #Join all the elements in the list and turn it into a String.
     print(f"{' '.join(display)}")
@@ -101,5 +110,7 @@ while not end_of_game:
         end_of_game = True
         print("You win.")
 
-    #TODO-3: - print the ASCII art from 'stages' that corresponds to the current number of 'lives' the user has remaining.
-
+    #TODO-3: - print the ASCII art from 'stages' that corresponds to the current number of 'lives' the user has remaining. 
+    
+    print(stages[lives])
+print()
